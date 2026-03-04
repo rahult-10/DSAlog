602e022c1f9109ff2fa76e3068d6266b9a451cc6
Rahul <rahult0710@gmail.com>
2026-03-04 21:31:06 +0530

    Time: 3 ms (32.56%), Space: 49.1 MB (14.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,44 +1,28 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-         List<String> result = new ArrayList<>();
+        List<String> result = new ArrayList<>();
         
         if (digits == null || digits.length() == 0)
             return result;
         
         String[] phone = {
-            "",
-            "",
-            "abc",
-            "def",
-            "ghi",
-            "jkl",
-            "mno",
-            "pqrs",
-            "tuv",
-            "wxyz"  
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
         };
         
-        combination(digits, 0, "", phone, result);
+        result.add("");
+        
+        for (char digit : digits.toCharArray()) {
+            List<String> temp = new ArrayList<>();
+            String letters = phone[digit - '0'];
+            for (String combination : result) {
+                for (char letter : letters.toCharArray()) {
+                    temp.add(combination + letter);
+                }
+            }
+            result = temp;
+        }
+        
         return result;
-    }
-    
-    private void combination(String digits, int index, 
-                            String current, String[] phone, 
-                            List<String> result) {
-                                
-        if (index == digits.length()) {
-            result.add(current);
-            return;
-        }
-        
-        String letters = phone[digits.charAt(index) - '0'];
-        
-        for (int i = 0; i < letters.length(); i++) {
-            combination(digits, 
-                       index + 1, 
-                       current + letters.charAt(i), 
-                       phone, 
-                       result);
-        }
     }
 }
