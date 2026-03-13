b700bfc105f7a278d560778ac8acf5dd0ce00f48
Rahul <rahult0710@gmail.com>
2026-03-14 00:01:41 +0530

    Time: 9 ms (72.73%), Space: 47.6 MB (88.89%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,26 +1,29 @@
 class Solution {
     public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-        int n = workerTimes.length;
-        long left = 0, right = (long)1e18;
+        long low = 1, right = (long)1e18, ans = right;
 
-        while (left < right) {
-            long mid = left + (right - left) / 2;
-            long totalHeight = 0;
-            for (int t : workerTimes) {
-                long lo = 0, hi = mountainHeight;
-                while (lo < hi) {
-                    long m = lo + (hi - lo + 1) / 2;
-                    if (m * (m + 1) / 2 <= mid / t) lo = m;
-                    else hi = m - 1;
-                }
-                totalHeight += lo;
+        while(low <= right){
+            long mid = (low + right) / 2;
+
+            long total = 0;
+
+            for(int x : workerTimes){
+                long val = (2 * mid) / x;
+                long k = (long)((Math.sqrt(1 + 4 * val) - 1) / 2);
+                total += k;
+
+                if(total >= mountainHeight) 
+                    break;
             }
-            if (totalHeight >= mountainHeight) {
-                right = mid;
-            } else {
-                left = mid + 1;
+
+            if(total >= mountainHeight){
+                ans = mid;
+                right = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 }
