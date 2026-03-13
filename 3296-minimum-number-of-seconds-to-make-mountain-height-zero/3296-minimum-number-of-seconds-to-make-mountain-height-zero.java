b0cef5153847bb1b14f7197c79ba9e1f3a406ffe
Rahul <rahult0710@gmail.com>
2026-03-13 23:58:43 +0530

    Time: 444 ms (5.05%), Space: 47.7 MB (70.71%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,26 +1,26 @@
 class Solution {
     public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
         int n = workerTimes.length;
-        PriorityQueue<long[]> pq = new PriorityQueue<>(
-            (a,b) -> Long.compare(a[0],b[0]));
-        for(int i = 0; i < n; i++){
-            pq.add(new long[]{workerTimes[i], i, 1});
-        }
-        long result = 0;
-        while(mountainHeight > 0){
-            long[] curr = pq.poll();
-            long tm = curr[0];
-            int id = (int)curr[1];
-            int x = (int)curr[2];
-            result = tm;
-            mountainHeight--;
+        long left = 0, right = (long)1e18;
 
-            if(mountainHeight > 0){
-                long nx = x + 1;
-                long nt = (long)workerTimes[id] * (nx * (nx+1)/2);
-                pq.add(new long[]{nt, id, nx});
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            long totalHeight = 0;
+            for (int t : workerTimes) {
+                long lo = 0, hi = mountainHeight;
+                while (lo < hi) {
+                    long m = lo + (hi - lo + 1) / 2;
+                    if (m * (m + 1) / 2 <= mid / t) lo = m;
+                    else hi = m - 1;
+                }
+                totalHeight += lo;
+            }
+            if (totalHeight >= mountainHeight) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return result;
+        return left;
     }
 }
