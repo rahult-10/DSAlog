99a2f1b351a3a06d0a87e32a222f06a9e1f3172d
Rahul <rahult0710@gmail.com>
2026-03-21 23:15:01 +0530

    Time: 44 ms (97.59%), Space: 42.9 MB (65.42%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,56 +1,64 @@
 class Solution {
+
+    boolean[][] rows = new boolean[9][10];
+    boolean[][] cols = new boolean[9][10];
+    boolean[][] boxes = new boolean[9][10];
+
     public void solveSudoku(char[][] board) {
-        sudokuSolver(board, 0, 0);
-    }
-    public boolean sudokuSolver(char[][] board, int row, int col){
-        if(row == 9){
-            return true;
+
+        // Step 1: Fill tracking arrays
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int digit = board[i][j] - '0';
+                    int box = (i / 3) * 3 + (j / 3);
+
+                    rows[i][digit] = true;
+                    cols[j][digit] = true;
+                    boxes[box][digit] = true;
+                }
+            }
         }
 
-        int nextRow = row;
-        int nextCol = col+1;
+        solve(board, 0, 0);
+    }
 
-        if(col == 8){
+    public boolean solve(char[][] board, int row, int col) {
+        if (row == 9) return true;
+
+        int nextRow = row;
+        int nextCol = col + 1;
+
+        if (col == 8) {
             nextRow = row + 1;
             nextCol = 0;
         }
-        if(board[row][col] != '.'){
-            return sudokuSolver(board, nextRow, nextCol);
+
+        if (board[row][col] != '.') {
+            return solve(board, nextRow, nextCol);
         }
-        for(int digit = 1; digit <= 9; digit++){
-            char ch = (char)(digit + '0');
-            if(isSafe(board, row, col, ch)){
-                board[row][col] = ch;
-                if(sudokuSolver(board, nextRow, nextCol)){
-                    return true;
-                }
+
+        int box = (row / 3) * 3 + (col / 3);
+
+        for (int digit = 1; digit <= 9; digit++) {
+            if (!rows[row][digit] && !cols[col][digit] && !boxes[box][digit]) {
+
+                // place
+                board[row][col] = (char)(digit + '0');
+                rows[row][digit] = true;
+                cols[col][digit] = true;
+                boxes[box][digit] = true;
+
+                if (solve(board, nextRow, nextCol)) return true;
+
+                // backtrack
                 board[row][col] = '.';
+                rows[row][digit] = false;
+                cols[col][digit] = false;
+                boxes[box][digit] = false;
             }
         }
+
         return false;
-    }
-    public boolean isSafe(char[][] board, int row , int col, int digit){
-        for(int i = 0; i < 9; i++){
-            if(board[i][col] == digit){
-                return false;
-            }
-        }
-
-        for(int j = 0; j < 9; j++){
-            if(board[row][j] == digit){
-                return false;
-            }
-        }
-
-        int sr = (row/3)*3;
-        int sc = (col/3)*3;
-        for(int i = sr; i < sr+3; i++){
-            for(int j = sc; j < sc + 3; j++){
-                if(board[i][j] == digit){
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
