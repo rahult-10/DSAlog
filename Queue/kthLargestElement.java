055f30e9a8abea80f3c5720fabdaf0752ea2952f
rahult0510 <1am23cd111@amceducation.in>
2026-05-18 23:26:54 +0530

    Kth Largest Element in array

--- Queue/kthLargestElement.java
+++ Queue/kthLargestElement.java
@@ -4,6 +4,23 @@ public int kthLargestElement(int[] nums, int k) {
     return nums[nums.length - k];
 }
 
+public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    for(int num : nums){
+
+        if(minHeap.size() < k){
+            minHeap.add(num);
+        }
+        else {
+            if(num > minHeap.peek()){
+                minHeap.poll();
+                minHeap.add(num);
+            }
+        }
+    }
+    return minHeap.peek();
+}
+
 public void main(String[] args) {
     int[] nums = {3,2,1,5,6,4};
     int k = 2;
