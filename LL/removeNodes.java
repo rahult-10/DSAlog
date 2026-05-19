7d200940be3c2f1a28dda6aca8fc8ad2c6c29514
rahult0510 <1am23cd111@amceducation.in>
2026-05-19 23:07:21 +0530

    2487. Remove Nodes From Linked List

--- LL/removeNodes.java
+++ LL/removeNodes.java
@@ -26,6 +26,31 @@ public ListNode removeNodes(ListNode head) {
     return curr;
 }
 
+public ListNode removeNode(ListNode head) {
+    ListNode prev = null;
+    ListNode curr = head;
+    while(curr != null){
+        ListNode nxt = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nxt;
+    }
+
+    curr = prev.next;
+    prev.next = null;
+
+    while(curr != null){
+        ListNode temp = curr.next;
+        if(curr.val >= prev.val){
+            curr.next = prev;
+            prev = curr;
+        }
+        curr = temp;
+    }
+
+    return prev;
+}
+
 public static void main(String[] args) {
     
 }
