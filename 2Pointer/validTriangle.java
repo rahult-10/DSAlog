a985312681bd80ca1c82fac8c83de30a8b201785
rahult0510 <1am23cd111@amceducation.in>
2026-05-21 00:43:16 +0530

    Valid Triangle Number

--- 2Pointer/validTriangle.java
+++ 2Pointer/validTriangle.java
@@ -15,8 +15,36 @@ public int triangleNumber(int[] nums) {
     return count;
 }
 
+public int validTriangle(int[] nums) {
+    Arrays.sort(nums);
+    int n = nums.length;
+    int count = 0;
+
+    for(int i = 0; i < n-2; i++){
+        for(int j = i+1; j < n-1; j++){
+            int left = j+1;
+            int right = n-1;
+            int idx = j;
+
+            while(left <= right){
+                int mid = left + (right-left)/2;
+                if(nums[i] + nums[j] > nums[mid]){
+                    idx = mid;
+                    left = mid+1;
+                }
+                else {
+                    right = mid-1;
+                }
+            }
+            count += (idx -j);
+        }
+    }
+    return count;
+}
+
 public void main(String[] args){
     int[] nums = {2,2,3,4};
     int res = triangleNumber(nums);
-    System.out.println(res);
+    int result = validTriangle(nums);
+    System.out.println(res + "," + result);
 }
