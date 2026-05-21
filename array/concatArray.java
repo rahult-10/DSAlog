d9ad539f12464d42928b47e1633167c0b2f2a721
rahult-10 <rahult0710@gmail.com>
2026-05-21 16:55:20 +0530

    Update

--- array/concatArray.java
+++ array/concatArray.java
@@ -15,6 +15,15 @@ public int[] getConcatenation(int[] nums) {
     return arr;
 }
 
+public int[] concatenation(int[] nums) {
+    int [] arr = new int [2*nums.length];
+    for(int i = 0; i < nums.length; i++){
+        arr[i] = nums[i];
+        arr[(arr.length-1)-i] = nums[(nums.length-1)-i];
+    }
+    return arr;
+}
+
 public void main(String[] args){
     int[] nums = {1,2,1};
     int[] concat = concatArray(nums);
