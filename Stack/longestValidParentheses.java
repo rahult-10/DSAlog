17986b0a677be210e867f2b941c017c1159f1052
rahult0510 <1am23cd111@amceducation.in>
2026-05-21 14:53:15 +0530

    Longest Valid Parentheses

---
+++ Stack/longestValidParentheses.java
@@ -0,0 +1,45 @@
+public int longestValidParentheses(String s) {
+    int left = 0;
+    int right = 0;
+    int max = 0;
+    for(int i = 0; i < s.length(); i++){
+        if(s.charAt(i) == '('){
+            left++;
+        }
+        else {
+            right++;
+        }
+        if(left == right){
+            max = Math.max(max, left*2);
+        }
+        else if(right > left){
+            left = 0;
+            right = 0;
+        }
+    }
+
+    left = 0;
+    right  = 0;
+    for(int i = s.length()-1; i >= 0; i--){
+        if(s.charAt(i) == '('){
+            left++;
+        }
+        else {
+            right++;
+        }
+        if(left == right){
+            max = Math.max(max, left*2);
+        }
+        else if(left > right){
+            left = 0;
+            right = 0;
+        }
+    }
+    return max;
+}
+
+public void main(String[] args){
+    String s = ")()())";
+    int count = longestValidParentheses(s);
+    System.out.println(count);
+}
