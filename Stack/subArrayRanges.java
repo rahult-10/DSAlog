e4820bcad6cf09590d7c7b4ac3fa8d2b37c4e32d
rahult-10 <rahult0710@gmail.com>
2026-05-22 11:55:13 +0530

    Sum Of SubArray Ranges

--- Stack/subArrayRanges.java
+++ Stack/subArrayRanges.java
@@ -12,6 +12,51 @@ public long subArrayRanges(int[] nums) {
     return sum;
 }
 
+public long sumSubArrayRanges(int[] nums) {
+    int n = nums.length;
+
+    long sumofMin = 0;
+    long sumofMax = 0;
+
+    // Sum Of Min
+    for (int i = 0; i < n; i++) {
+        int countLeft = 1;
+        for (int j = i - 1; j >= 0; j--) {
+            if (nums[j] > nums[i]) countLeft++;
+            else break;
+        }
+
+        int countRight = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (nums[j] >= nums[i]) countRight++;
+            else break;
+        }
+
+        sumofMin += (long) countLeft * countRight * nums[i];
+    }
+
+    // Sum of Max
+    for (int i = 0; i < n; i++) {
+        int countLeft = 1;
+        for (int j = i - 1; j >= 0; j--) {
+            if (nums[j] < nums[i]) countLeft++;
+            else break;
+        }
+
+        int countRight = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (nums[j] <= nums[i]) countRight++;
+            else break;
+        }
+
+        sumofMax += (long) countLeft * countRight * nums[i];
+    }
+
+    return sumofMax - sumofMin;
+}
+
+
+
 public void main(String[] args) {
     int nums[] = {1,2,3};
     long res = subArrayRanges(nums);
