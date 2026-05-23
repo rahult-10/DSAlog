9cdc26848ce5de7f4ac38e49f353591a36b4656c
rahult-10 <rahult0710@gmail.com>
2026-05-23 21:39:35 +0530

    Maximum Product

--- array/maximumProd.java
+++ array/maximumProd.java
@@ -12,6 +12,18 @@ public int maximumProd(int[] nums) {
     return maxProd;
 }
 
+
+public int maximumProduct(int[] nums) {
+    int n = nums.length;
+    Arrays.sort(nums);
+    int prod1 = nums[n-1]*nums[n-2]*nums[n-3];
+    int prod2 = nums[0]*nums[1]*nums[n-1];
+
+    int maxProd = Math.max(prod1, prod2);
+    return maxProd;
+}
+
+
 public void main(String[] arges){
     int[] nums = {1,2,3};
     int res = maximumProd(nums);
