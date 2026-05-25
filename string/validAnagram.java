542f2964e958d02ffa9a18c50af7c66450fe6335
rahult-10 <rahult0710@gmail.com>
2026-05-25 23:48:13 +0530

    Valid Anagram

---
+++ string/validAnagram.java
@@ -0,0 +1,38 @@
+public boolean validAnagram(String s, String t) {
+    if(s.length() != t.length()){
+        return false;
+    }
+
+    Map<Character, Integer> map = new HashMap<>();
+
+    for(int i = 0; i < s.length(); i++){
+        char ch = s.charAt(i);
+
+        map.put(ch, map.getOrDefault(ch, 0)+1);
+    }
+
+    for(int i = 0; i < t.length(); i++){
+        char ch = t.charAt(i);
+
+        if(!map.containsKey(ch)){
+            return false;
+        }
+
+        map.put(ch, map.get(ch)-1);
+
+        if(map.get(ch) == 0){
+            map.remove(ch);
+        }
+    }
+
+    return map.isEmpty();
+}
+
+public void main(String[] args){
+    String s  = "anagram";
+    String t = "nagrama";
+
+    boolean res = validAnagram(s, t);
+
+    System.out.println(res);
+}
